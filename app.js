5209593e6d3cba5c0d025490d37f9125ebcd6929
Jaswant Kushwaha <114646646+jaswant2111058@users.noreply.github.com>
2023-04-29 23:38:09 +0530

    v4

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-      origin: "*",  
+      origin: "https://videochatbyjassi.onrender.com",  
       methods: ["GET", "POST"]
     }
 })
@@ -43,7 +43,7 @@ mongoose.connect(process.env.DB_URI, {
     .catch(err => console.log(err));
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://videochatbyjassi.onrender.com",
     })
 );
 
